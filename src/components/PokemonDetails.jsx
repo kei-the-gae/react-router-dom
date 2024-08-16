062902b7a32f157ff880d0d914a189f0e1e73439
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-08-16 09:58:30 -0700

    Add details page component and dynamic link to list page

---
+++ src/components/PokemonDetails.jsx
@@ -0,0 +1,16 @@
+const PokemonDetails = (props) => {
+    console.log(props);
+    return (
+        <>
+            <h2>Pokemon Details</h2>
+            <dl>
+                <dt>Weight: </dt>
+                <dd></dd>
+                <dt>Height: </dt>
+                <dd></dd>
+            </dl>
+        </>
+    );
+};
+
+export default PokemonDetails;
